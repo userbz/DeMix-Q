a438c97d3a3e85facd05c6000874996386f3295d
userbz <bo.zhang@ki.se>
2017-02-16 18:04:15 +0100

    py3.5 mzid2idxml

--- scripts/mzid2idxml_theomass.py
+++ scripts/mzid2idxml_theomass.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 import numpy
 import os
@@ -5,6 +6,8 @@ import lxml.etree as et
 from pyteomics import mzid
 from pprint import pprint
 # Bo Zhang 2015.01.12 : Use theoretical m/z
+# Python3
+
 
 def _cleanup(e):
     e.clear()
@@ -17,15 +20,13 @@ def _dbrefs(fn):
     refDict = dict()
     protRef = dict()
     xml = et.iterparse(fn, events=('start', 'end'))
-
     for i, e in xml:
         if e.tag == ns + 'DBSequence' and i == 'end':
             protRef[e.get('id')] = e.get('accession')
             _cleanup(e)
-
         elif e.tag == ns + 'Peptide' and i == 'end':
             pepid = e.get('id')
-            baseseq = e.iter(ns + 'PeptideSequence').next().text
+            baseseq = next(e.iter(ns + 'PeptideSequence')).text
             peplist = [''] + list(baseseq)
             for mod in e.iter(ns + 'Modification'):
                 pos = int(mod.get('location'))
@@ -34,60 +35,50 @@ def _dbrefs(fn):
             pepseq = ''.join(peplist)
             refDict[pepid] = (baseseq, pepseq)
             _cleanup(e)
-
         elif e.tag == ns + 'PeptideEvidence' and i == 'end':
             flag = e.get('isDecoy') == 'false' and 1 or -1
-            refDict[e.get('id')] = (
-                flag, e.get('pre'), e.get('post'), protRef[e.get('dBSequence_ref')])
+            refDict[e.get('id')] = (flag, e.get('pre'), e.get('post'), protRef[e.get('dBSequence_ref')])
             _cleanup(e)
-
         elif e.tag == ns + 'SequenceCollection' and i == 'end':
             del xml
             return refDict
 
 
-def mzid2pin(fn):
+def mzid2pin(fn, max_charge=7):
+    fh = open(fn + '.idxml', 'w')
     db = _dbrefs(fn)
     psmReader = mzid.read(fn)
-    sys.stdout = open(fn + '.idxml', 'wb')
-
-    print r'''<?xml version="1.0" encoding="UTF-8"?>
+    print( r'''<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="http://open-ms.sourceforge.net/XSL/IdXML.xsl" ?>
 <IdXML version="1.2" xsi:noNamespaceSchemaLocation="http://open-ms.sourceforge.net/SCHEMAS/IdXML_1_2.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <SearchParameters id="SP_0" db="" db_version="" taxonomy="" mass_type="monoisotopic" charges="" enzyme="Trypsin/P" missed_cleavages="3" precursor_peak_tolerance="10" peak_mass_tolerance="20" ></SearchParameters>
-    <IdentificationRun date="" search_engine="" search_engine_version="" search_parameters_ref="SP_0" > '''
+    <IdentificationRun date="" search_engine="" search_engine_version="" search_parameters_ref="SP_0" > ''', file=fh)
 
     for psm in psmReader:
-        if psm.has_key('SpectrumIdentificationItem'):
-            specId = psm['spectrumID']
-            specTitle = psm['spectrum title'].split(':')
+        if 'SpectrumIdentificationItem' not in psm:
+            continue
+        rt = psm['scan start time']
+        for match in psm['SpectrumIdentificationItem']:
+            if match['MS-GF:QValue'] > 0.01:
+            # if match['MS-GF:PepQValue'] > 0.01:
+                continue
+            z = match['chargeState']
+            if not 2 <= z <= max_charge:
+                continue
+            expMZ = match['experimentalMassToCharge']
+            mz = match['calculatedMassToCharge']  # - (expMZ - featMZ)
+            score = match['MS-GF:EValue']
 
-            featMZ = float(specTitle[1])
-            rt = specTitle[2][:-1]
-
-            for match in psm['SpectrumIdentificationItem']:
-                if match['MS-GF:QValue'] > 0.01:
-                # if match['MS-GF:PepQValue'] > 0.01:
-                    continue
-
-                z = match['chargeState']
-                if not 2 <= z <= 4:
-                    continue
-
-                expMZ = match['experimentalMassToCharge']
-                mz = match['calculatedMassToCharge']  # - (expMZ - featMZ)
-                score = match['MS-GF:EValue']
-
-                flag, enzN, enzC, prot = db[match['PeptideEvidenceRef'][0]['peptideEvidence_ref']]
-                pepref = match['peptide_ref']
-                bseq, seq = db[pepref]
-
-                print ''' <PeptideIdentification score_type="" higher_score_better="false" significance_threshold="0" MZ="%f" RT="%s" >
+            flag, enzN, enzC, prot = db[match['PeptideEvidenceRef'][0]['peptideEvidence_ref']]
+            pepref = match['peptide_ref']
+            bseq, seq = db[pepref]
+            print(''' <PeptideIdentification score_type="" higher_score_better="false" significance_threshold="0" MZ="%f" RT="%s" >
             <PeptideHit score="%f" sequence="%s" charge="%d" aa_before="%s" aa_after="%s"></PeptideHit>
         </PeptideIdentification>
-                ''' % (mz, rt, score, seq, z, enzN, enzC)
+                ''' % (mz, rt, score, seq, z, enzN, enzC), file=fh)
 
-    print '\n</IdentificationRun>\n</IdXML>'
+    print('\n</IdentificationRun>\n</IdXML>',  file=fh)
+
 
 if __name__ == '__main__':
     for fn in sys.argv[1:]:
